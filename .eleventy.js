3ca91b2c82bd5e1054c93b3b45316d68f8d6965b
arnoloog <arnovanbunder@gmail.com>
2025-11-22 12:36:36 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Passthrough files
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/photos");
+
+  // Nunjucks date filter
+  eleventyConfig.addNunjucksFilter("date", (value, format = "yyyy-MM-dd") => {
+    try {
+      return DateTime.fromJSDate(value).toFormat(format);
+    } catch (e) {
+      console.error("Date filter error:", e);
+      return value;
+    }
+  });
 
   return {
     dir: {
