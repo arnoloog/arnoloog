ccc4efa9cb7b550c33be63c2e9f976c9bfa22c18
arnoloog <arnovanbunder@gmail.com>
2025-11-22 14:54:37 +0100

    Update photos.js

--- src/_data/photos.js
+++ src/_data/photos.js
@@ -1,36 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
-function getImagesFromDir(relativeDir) {
-  const inputDir = path.join(__dirname, "..", "photos", relativeDir);
-  if (!fs.existsSync(inputDir)) return [];
-  return fs.readdirSync(inputDir)
-    .filter(file => file.match(/\.(jpe?g|png|webp|gif)$/i))
-    .sort()
-    .map(file => ({
-      src: `/photos/${relativeDir}/${file}`,
-      filename: file
+module.exports = function () {
+  const rollsDir = path.join(__dirname, "..", "photos", "rolls");
+  const rolls = {};
+
+  // read all roll folders
+  fs.readdirSync(rollsDir).forEach((rollFolder) => {
+    const folderPath = path.join(rollsDir, rollFolder);
+    if (!fs.lstatSync(folderPath).isDirectory()) return;
+
+    // get images inside roll
+    const images = fs
+      .readdirSync(folderPath)
+      .filter((f) => /\.(jpg|jpeg|png|webp)$/i.test(f))
+      .sort(); // ensures 01,02,03 order
+
+    rolls[rollFolder] = images.map((img) => ({
+      src: `/photos/rolls/${rollFolder}/${img}`,
+      filename: img,
     }));
-}
-
-module.exports = function() {
-  const homepage = getImagesFromDir("homepage");
-
-  const rollsRoot = path.join(__dirname, "..", "photos", "rolls");
-  let rolls = [];
-  if (fs.existsSync(rollsRoot)) {
-    const entries = fs.readdirSync(rollsRoot, { withFileTypes: true });
-    rolls = entries
-      .filter(entry => entry.isDirectory())
-      .map(dir => {
-        const slug = dir.name;
-        const photos = getImagesFromDir(path.join("rolls", slug));
-        return { slug, photos };
-      });
-  }
+  });
 
   return {
-    homepage,
-    rolls
+    rolls,
+    rollsList: Object.keys(rolls).sort(),
   };
 };
